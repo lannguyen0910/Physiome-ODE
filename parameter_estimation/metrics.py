e3c29a5b4f24750edcdbeb84b9df4bebfe72577f
lannguyen <nguyen006@uni-hildesheim.de>
2026-08-23 00:36:43 +0200

    Initialize eval metrics

---
+++ parameter_estimation/metrics.py
@@ -0,0 +1,83 @@
+import torch
+
+
+class ParameterMetrics:
+    @staticmethod
+    def mae(
+        prediction,
+        target
+    ):
+        return torch.mean(
+            torch.abs(
+                prediction - target
+            )
+        )
+
+    @staticmethod
+    def rmse(
+        prediction,
+        target
+    ):
+        return torch.sqrt(
+            torch.mean(
+                (
+                    prediction
+                    - target
+                ) ** 2
+            )
+        )
+
+    @staticmethod
+    def relative_error(
+        prediction,
+        target,
+        eps=1e-8
+    ):
+        return torch.mean(
+            torch.abs(
+                prediction - target
+            )
+            /
+            (torch.abs(target) + eps)
+        )
+
+    @staticmethod
+    def log_mae(
+        prediction,
+        target
+    ):
+        return torch.mean(
+            torch.abs(
+                torch.log(prediction)
+                -
+                torch.log(target)
+            )
+        )
+
+
+class TrajectoryMetrics:
+
+    @staticmethod
+    def mae(
+        prediction,
+        target
+    ):
+        return torch.mean(
+            torch.abs(
+                prediction - target
+            )
+        )
+
+    @staticmethod
+    def rmse(
+        prediction,
+        target
+    ):
+        return torch.sqrt(
+            torch.mean(
+                (
+                    prediction
+                    - target
+                ) ** 2
+            )
+        )
